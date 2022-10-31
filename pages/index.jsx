0e2d1efcf9bd7b29531ec93bfadafe8fc3fc8760
danielcalvolopez <acidistrict@gmail.com>
2022-10-31 17:08:28 +0000

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,46 @@
+import { MongoClient } from "mongodb";
+import MeetupList from "../components/meetups/MeetupList";
+
+import Head from "next/head";
+
+const HomePage = (props) => {
+  return (
+    <>
+      <Head>
+        <title>Meetups App</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
+};
+
+export const getStaticProps = async () => {
+  const client = await MongoClient.connect(
+    "mongodb+srv://user1:LCORrTPeVKDsltYb@cluster0.poqsfen.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+};
+
+export default HomePage;
